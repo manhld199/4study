2f34a5636a3b1fa9608203df9db04a6132301022
Lamcam <lamcam813@gmail.com>
2024-11-19 00:57:10 +0700

    update(course-detail): update recommended course

--- src/components/(general)/recommended-courses/index.tsx
+++ src/components/(general)/recommended-courses/index.tsx
@@ -1,31 +1,42 @@
-import { useEffect, useState } from "react";
-import { CardCourse } from "@/components";
-import { useRouter } from "next/navigation";
+"use client";
+// import libs
 import { ChevronRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import components
+import { CardCourse } from "@/components";
 
 export default function RecommendedCourses() {
-  const [popularCourses, setPopularCourses] = useState([]);
   const router = useRouter();
+  const [otherCourses, setOtherCourses] = useState<Course[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   useEffect(() => {
-    // Fetch the popular courses data
-    const fetchPopularCourses = async () => {
+    const fetchOtherCourses = async () => {
       try {
-        const popularResponse = await fetch("/api/courses/popularity", {
+        setLoading(true);
+        const response = await fetch("/api/courses/all-course", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
         });
-        const popularData = await popularResponse.json();
-
-        const top4Courses = popularData.data.slice(0, 4);
-        setPopularCourses(top4Courses);
+        const data = await response.json();
+        setOtherCourses(data.data || []);
       } catch (error) {
-        console.error("Error fetching popular courses:", error);
+        console.error("Error fetching other courses:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchPopularCourses();
+    fetchOtherCourses();
   }, []);
 
   const handleSeeMoreClick = () => {
@@ -37,12 +48,45 @@ export default function RecommendedCourses() {
       <p className="text-white text-[36px] font-semibold mb-3">
         You may also like
       </p>
-      {/* Render top 4 popular courses */}
-      <div className="grid grid-cols-4 gap-12">
-        {popularCourses.map((course, index) => (
-          <CardCourse key={`course-card-${course._id}`} course={course} />
-        ))}
-      </div>
+
+      {/* Slider for other courses */}
+      {loading ? (
+        <div className="grid grid-cols-4 gap-12">
+          {[...Array(4)].map((_, index) => (
+            <Skeleton
+              key={index}
+              height={350}
+              className="rounded-[10px] skeleton-custom"
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="relative">
+          <Swiper
+            modules={[Navigation, Autoplay]}
+            spaceBetween={15}
+            slidesPerView={4}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}>
+            {otherCourses.map((course, index) => (
+              <SwiperSlide key={`course-card-${course._id}`}>
+                <CardCourse course={course} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {/* Custom navigation buttons */}
+          <div className="swiper-button-next text-white"></div>
+          <div className="swiper-button-prev text-white"></div>
+        </div>
+      )}
+
       <div
         onClick={handleSeeMoreClick}
         className="flex items-center justify-center cursor-pointer text-white">
